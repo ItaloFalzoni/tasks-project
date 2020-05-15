ddeca205fe6c90c583db9df407e75c5940f8d6fd
Ítalo Falzoni <italo.falzoni@gmail.com>
2020-05-15 11:59:32 -0300

    Projeto Finalizado

--- mobile/src/common.js
+++ mobile/src/common.js
@@ -7,7 +7,7 @@ function showError(err) {
   if (err.response && err.response.data) {
     Alert.alert('Ops! Ocorreu um problema!', `Mensagem: ${err.response.data}`)
   } else {
-    Alert.alert('Ops! Ocorreu um problema!', `Mensagem: ${err.response.data}`)
+    Alert.alert('Ops! Ocorreu um problema!', `Mensagem: ${err}`)
   }
 }
 
